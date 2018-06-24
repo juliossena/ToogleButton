e2ce3d68aff9268ac565bc6452cc9f21c4f5ed28
juliossena <juliosouzasena@gmail.com>
2018-06-23 21:54:03 -0300

    final

--- app/src/main/java/tooglebutton/cursoandroid/dell/tooglebutton/MainActivity.java
+++ app/src/main/java/tooglebutton/cursoandroid/dell/tooglebutton/MainActivity.java
@@ -1,13 +1,33 @@
 package tooglebutton.cursoandroid.dell.tooglebutton;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private ToggleButton toggle;
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toggle = (ToggleButton) findViewById(R.id.toggleButton_id);
+        resultado = (TextView) findViewById(R.id.resultado_id);
+
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                resultado.setText("");
+                if (b) {
+                    resultado.setText("Mostrar...");
+                }
+            }
+        });
     }
 }
